ddddbd5c22deb5de57345306edf4538fc43be300
Vladimir Kravchenko <vladimirkravchenko@Vladimirs-MacBook-Air.local>
2018-07-12 20:23:23 +0300

    Instagram Component is created

--- source/index.js
+++ source/index.js
@@ -2,22 +2,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Pages
+import Instagram from './pages/instagram';
+
 // Theme
 import './theme/init';
 
 const start = (
     <h1
-        style = { {
-            display:         'flex',
-            justifyContent:  'center',
-            alignItems:      'center',
-            minHeight:       '100vh',
+        style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            minHeight: '100vh',
             backgroundColor: '#070A13',
-            color:           'white',
-            fontSize:        24,
-            fontWeight:      '600',
-        } }>
+            color: 'white',
+            fontSize: 24,
+            fontWeight: '600',
+        }}>
         Добро пожаловать на воркшоп по React.
+            <Instagram />
     </h1>
 );
 
---
+++ source/pages/instagram/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Instagram extends React.Component {
+    render() {
+        return (
+            <section>
+                <p>Instagram Header</p>
+                <p>Instagram Content</p>
+            </section>
+        )
+    }
+}
+
+export default Instagram;
