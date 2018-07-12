4d7906d8146d27641b2a5516689995ef14328f7d
Vladimir Kravchenko <vladimirkravchenko@Vladimirs-MacBook-Air.local>
2018-07-12 20:39:36 +0300

    Instagram content is added

---
+++ source/components/instagram/content/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Content extends React.Component {
+    render() {
+        return (
+            <div className="content">
+                <div>Profile</div>
+                <div>Photos</div>
+            </div>
+        );
+    }
+}
+
+export default Content;
--- source/pages/instagram/index.js
+++ source/pages/instagram/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Header from '../../components/instagram/header';
+import Content from '../../components/instagram/content';
 
 class Instagram extends React.Component {
     render() {
         return (
             <div>
                 <Header/>
+                <Content/>
             </div>
         )
     }
